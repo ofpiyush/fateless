60148a6acc0c02238a1e4d6933aef21d4b196fd1
Piyush Mishra <me@piyushmishra.com>
2011-04-24 02:00:56 +0530

    config.sample.php added

---
+++ config.sample.php
@@ -0,0 +1,12 @@
+<?php
+
+	$config = array
+		( 
+			'server'		=> 'chat.freenode.net',
+			'port'			=> 6667,
+			'channels'		=> array(),
+			'name'			=> '',
+			'nick'			=> '',
+			'pass'			=> null,
+			'readLength'	=> 512
+		);
