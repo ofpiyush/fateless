8d169ecd925c6a53321e6254aa8fdf4cf0178aa1
Piyush Mishra <me@piyushmishra.com>
2011-04-28 14:22:17 +0530

    new file:   cache/index.html 	new file:   engine/core/cache.php 	new file:   logs/default.txt 	new file:   logs/piyushmishra

---
+++ engine/core/cache.php
@@ -0,0 +1,35 @@
+<?php
+if ( ! defined('FATELESS_ENGINEPATH')) exit('No direct script access allowed');
+/**
+ * fateless
+ * Copyright (C) 2010-2011  Piyush Mishra
+ *
+ * License:
+ * This file is part of fateless (http://github.com/piyushmishra/fateless)
+ * 
+ * fateless is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * fateless is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with fateless.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package fateless
+ * @author Piyush Mishra <me[at]piyushmishra[dot]com>
+ * @license http://www.gnu.org/licenses/gpl.html
+ * @copyright 2010-2011 Piyush Mishra
+ */
+class cache
+{
+	function __construct()
+	{
+		
+	}
+	
+}
